b06307c96e7cb91912cecb965f053ed47025f2f7
Diego Bonilla <diegob8846@gmail.com>
2025-07-24 15:13:05 -0400

    Used i2c_master_transmit_receive to read the WHO_AM_I register from the MPU6050. - Confirmed that the sensor responded at address 0x68, verifying proper connection and addressing. - Resolved errors to allow code to build.

--- read-from-sensor/main/read-from-sensor.c
+++ read-from-sensor/main/read-from-sensor.c
@@ -2,6 +2,11 @@
 #include "driver/i2c_master.h"
 #include "esp_log.h"
 
+#define MPU_ADDR         0x68
+#define WHO_AM_I_REG     0x75
+#define PWR_MGMT_1_REG   0x6B
+
+
 void app_main(void) {
 	
 	i2c_master_bus_handle_t bus_handle; 	
@@ -15,16 +20,26 @@ void app_main(void) {
 		.flags.enable_internal_pullup = true,
 	};
 
-	ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &bus_handle)));
+	ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &bus_handle));
 
-	i2c_device_handle_t dev_handle;
+	i2c_master_dev_handle_t dev_handle;
 
 	i2c_device_config_t dev_config = {
-		dev_addr_length = I2C_ADDR_BIT_LEN_7,
-		.device_address = I2C_ADDRESS,
-		.scl_speed_hz = 1000000;
-	}
+		.dev_addr_length = I2C_ADDR_BIT_LEN_7,
+		.device_address = MPU_ADDR,
+		.scl_speed_hz = 400000,
+	};
 
 
-	ESP_ERROR_CHECK(i2c_new_master_bus_add_device(bus_config, &dev_config, &dev_handle)));
+	ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_config, &dev_handle));
+
+	uint8_t wake_up[2]  = { PWR_MGMT_1_REG, 0x00};
+
+	ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, wake_up, sizeof(wake_up), -1));
+
+	uint8_t Who_am_i_reg = WHO_AM_I_REG;
+	uint8_t who_am_i = 0;
+
+ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, &Who_am_i_reg, 1, &who_am_i, 1, -1));
+	ESP_LOGI("MPU6050", "who_am_i = 0x%X", who_am_i);
 }
