e58bf6bc58866ffe5bf6bec06b238294d0397645
Diego Bonilla <diegob8846@gmail.com>
2025-07-25 20:03:03 -0400

    Moved sensor pasring code into a task

--- read-from-sensor/main/read-from-sensor.c
+++ read-from-sensor/main/read-from-sensor.c
@@ -2,6 +2,8 @@
 #include "driver/i2c_master.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <math.h>
 
 #define MPU_ADDR         	0x68
 #define WHO_AM_I_REG     	0x75
@@ -14,10 +16,51 @@
 #define LSB_PER_G               16384.0
 #define LSB_PER_DEG             131.0
 
+i2c_master_dev_handle_t dev_handle;
+
 void sendData(i2c_master_dev_handle_t *dev_handle, uint8_t addr, uint8_t val){
 	
 	uint8_t data[] = { addr, val };
 	ESP_ERROR_CHECK(i2c_master_transmit(*dev_handle, data, sizeof(data), -1));
+}
+
+
+void handleData( void *parameter) {
+	
+	while(1){
+	
+		uint8_t sensor_data[14];  // accelX, accelY, accelZ, temp, gyroX, gyroY, gyroZ
+
+		uint8_t reg_addr = 0x3B;
+		ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, &reg_addr, 1, sensor_data, 14, -1));
+
+		// Parse the raw values
+		int16_t raw_accel_x = (sensor_data[0] << 8) | sensor_data[1];
+		int16_t raw_accel_y = (sensor_data[2] << 8) | sensor_data[3];
+		int16_t raw_accel_z = (sensor_data[4] << 8) | sensor_data[5];
+
+		int16_t temp_raw = (sensor_data[6] << 8) | sensor_data[7];
+
+		int16_t raw_gyro_x = (sensor_data[8]  << 8) | sensor_data[9];
+		int16_t raw_gyro_y = (sensor_data[10] << 8) | sensor_data[11];
+		int16_t raw_gyro_z = (sensor_data[12] << 8) | sensor_data[13];
+
+		float g_force_x = (float)raw_accel_x / LSB_PER_G;
+		float g_force_y = (float)raw_accel_y / LSB_PER_G;
+		float g_force_z = (float)raw_accel_z / LSB_PER_G;
+
+		float ang_velocity_x = (float)raw_gyro_x / LSB_PER_DEG;
+		float ang_velocity_y = (float)raw_gyro_y / LSB_PER_DEG;
+		float ang_velocity_z = (float)raw_gyro_z / LSB_PER_DEG;
+
+		ESP_LOGI("MPU6050", "Accel: X=%f, Y=%f, Z=%f", g_force_x, g_force_y, g_force_z);
+		ESP_LOGI("MPU6050", "Gyro:  X=%f, Y=%f, Z=%f", ang_velocity_x, ang_velocity_y, ang_velocity_z);
+
+		
+				
+	}
+
+
 }
 
 
@@ -37,7 +80,6 @@ void app_main(void) {
 
 	ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &bus_handle));
 
-	i2c_master_dev_handle_t dev_handle;
 
 	i2c_device_config_t dev_config = {
 		.dev_addr_length = I2C_ADDR_BIT_LEN_7,
@@ -69,36 +111,8 @@ void app_main(void) {
 
 	sendData(&dev_handle, ACCEL_CONFIG_REG, 0x00); // Accel full scale range
 
-	while(1){
-	
-		uint8_t sensor_data[14];  // accelX, accelY, accelZ, temp, gyroX, gyroY, gyroZ
 
-		uint8_t reg_addr = 0x3B;
-		ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, &reg_addr, 1, sensor_data, 14, -1));
+	xTaskCreatePinnedToCore(&handleData, "handling data", 3072, NULL, 1, NULL, 1);
 
-		// Parse the raw values
-		int16_t raw_accel_x = (sensor_data[0] << 8) | sensor_data[1];
-		int16_t raw_accel_y = (sensor_data[2] << 8) | sensor_data[3];
-		int16_t raw_accel_z = (sensor_data[4] << 8) | sensor_data[5];
-
-		int16_t temp_raw = (sensor_data[6] << 8) | sensor_data[7];
-
-		int16_t raw_gyro_x = (sensor_data[8]  << 8) | sensor_data[9];
-		int16_t raw_gyro_y = (sensor_data[10] << 8) | sensor_data[11];
-		int16_t raw_gyro_z = (sensor_data[12] << 8) | sensor_data[13];
-
-		float g_force_x = (float)raw_accel_x / LSB_PER_G;
-		float g_force_y = (float)raw_accel_y / LSB_PER_G;
-		float g_force_z = (float)raw_accel_z / LSB_PER_G;
-
-		float ang_velocity_x = (float)raw_gyro_x / LSB_PER_DEG;
-		float ang_velocity_y = (float)raw_gyro_y / LSB_PER_DEG;
-		float ang_velocity_z = (float)raw_gyro_z / LSB_PER_DEG;
-
-		ESP_LOGI("MPU6050", "Accel: X=%f, Y=%f, Z=%f", g_force_x, g_force_y, g_force_z);
-		ESP_LOGI("MPU6050", "Gyro:  X=%f, Y=%f, Z=%f", ang_velocity_x, ang_velocity_y, ang_velocity_z);
-
-		
-	}
 }
 
